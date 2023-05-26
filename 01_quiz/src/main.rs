e01c207c8cfcba59c6d4506a7c5ba1a5dbae2bcb
Benjamin Eckardt <benjamin.eckardt@beneck.de>
2023-05-27 01:40:16 +0200

    Adds timer functionality.

--- 01_quiz/src/main.rs
+++ 01_quiz/src/main.rs
@@ -1,12 +1,19 @@
 use std::{error::Error, fs, io, path::PathBuf, process};
+use std::thread;
+use std::sync::mpsc::{Sender, Receiver, TryRecvError};
+use std::sync::mpsc;
+use std::time::Instant;
 
 use clap::Parser;
 use serde::Deserialize;
 
 #[derive(Parser)]
 struct Cli {
-    #[arg(long)]
+    #[arg(short = 'p', long = "path")]
     path: PathBuf,
+
+    #[arg(short = 's', long = "seconds", default_value_t = 30)]
+    seconds: u64,
 }
 
 #[derive(Debug, Deserialize)]
@@ -15,25 +22,44 @@ struct QuizEntry {
     answer: String,
 }
 
-fn quiz(quiz_entries: &Vec<QuizEntry>) -> Result<(), Box<dyn Error>> {
+fn quiz(quiz_entries: &Vec<QuizEntry>, time_limit_in_seconds: u64) -> Result<(), Box<dyn Error>> {
+    let instant = Instant::now();
     let mut correct_count = 0;
-    let mut incorrect_count = 0;
+    let overall_count = quiz_entries.len();
 
-    let mut guess = String::new();
-    for quiz_entry in quiz_entries {
-        println!("{}", quiz_entry.question);
-        io::stdin().read_line(&mut guess).expect("Failed to read line from stdin");
-        if guess.trim().eq(&quiz_entry.answer) {
-            println!("Correct!");
-            correct_count += 1;
-        } else {
-            println!("Wrong! The answer to {} is {}, not {}", quiz_entry.question, quiz_entry.answer, guess.trim());
-            incorrect_count += 1
+    'outer: for quiz_entry in quiz_entries {
+        let (tx, rx): (Sender<String>, Receiver<String>) = mpsc::channel();
+        let question = quiz_entry.question.clone();
+        thread::spawn(move || {
+            let mut guess = String::new();
+            println!("{}", question);
+            io::stdin().read_line(&mut guess).expect("Failed to read line from stdin");
+            tx.send(guess.clone()).unwrap();
+            guess.clear();
+        });
+        loop {
+            if instant.elapsed().as_secs() > time_limit_in_seconds {
+                println!("Time is up");
+                break 'outer;
+            }
+            match rx.try_recv() {
+                Err(TryRecvError::Empty) => {},
+                Err(_error) => {
+                    panic!("Error when trying to receive answer")
+                },
+                Ok(answer) =>{
+                    if answer.trim().eq(&quiz_entry.answer) {
+                        println!("Correct!");
+                        correct_count += 1;
+                    } else {
+                        println!("Wrong! The answer to {} is {}, not {}", quiz_entry.question, quiz_entry.answer, answer.trim());
+                    }
+                    break;
+                }
+            }
         }
-        guess.clear();
     }
 
-    let overall_count = correct_count + incorrect_count;
     println!("Result: {}/{} correct", correct_count, overall_count);
 
     Ok(())
@@ -55,7 +81,7 @@ fn main() {
     let quiz_entries = parse_csv(content)
         .unwrap_or_else(|e| panic!("Failed to parse csv file: {:?} due to {}", args.path, e));
 
-    if let Err(err) = quiz(&quiz_entries) {
+    if let Err(err) = quiz(&quiz_entries, args.seconds) {
         eprintln!("Error during quiz: {}", err);
         process::exit(1);
     }
